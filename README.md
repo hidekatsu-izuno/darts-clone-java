6a587ea412313fe044d104dce6379e2599831fe9
Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
2018-06-16 19:55:14 +0900

    Add license headers

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # darts-clone-java
+
 Double-ARray Trie System clone written in Java
+
+This library is based on [Darts Clone](https://github.com/s-yata/darts-clone).
--- src/main/java/net/arnx/dartsclone/internal/DawgBuilder.java
+++ src/main/java/net/arnx/dartsclone/internal/DawgBuilder.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2018 Hidekatsu Izuno <hidekatsu.izuno@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.arnx.dartsclone.internal;
 
 import net.arnx.dartsclone.util.BooleanList;
@@ -7,7 +22,7 @@ public class DawgBuilder {
 	private static final int INITIAL_TABLE_SIZE = 1 << 10;
 	
 	private static int hash(int key) {
-		key = ~key + (key << 15);  // key = (key << 15) - key - 1;
+		key = ~key + (key << 15);
 		key = key ^ (key >> 12);
 		key = key + (key << 2);
 		key = key ^ (key >> 4);
